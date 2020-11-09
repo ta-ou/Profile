e9178824488d12be737c3cb62602208850b0619c
Takuya <takuyaouchi18@gmail.com>
2020-11-09 18:52:02 +0700

    footer import

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Header />
+    <Footer />
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
+    Footer
   }
 }
 </script>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,42 @@
 <template>
-  <footer>
-    <ul class="footer-menu">
-      <li>Home ｜</li>
-      <li>About ｜</li>
-      <li>Works ｜</li>
-      <li>Contact</li>
-    </ul>
-    <p>© All rights reserved by Takuya</p>
+  <footer class="page-footer font-small">
+    <div class="container">
+      <div class="row text-center d-flex justify-content-center pt-5 mb-3">
+        <div class="col-md-2 mb-3">
+          <h6 class="font-weight-bold">
+            <a href="#!">Home</a>
+          </h6>
+        </div>
+        <div class="col-md-2 mb-3">
+          <h6 class="font-weight-bold">
+            <a href="#!">About</a>
+          </h6>
+        </div>
+        <div class="col-md-2 mb-3">
+          <h6 class="font-weight-bold">
+            <a href="#!">Works</a>
+          </h6>
+        </div>
+        <div class="col-md-2 mb-3">
+          <h6 class="font-weight-bold">
+            <a href="#!">Contact</a>
+          </h6>
+        </div>
+      </div>
+      <!-- Copyright -->
+      <div class="footer-copyright text-center py-3">
+        © 2020 Copyright:
+        <a href="">Takuya's portforio</a>
+      </div>
+    </div>
+    <!-- Copyright -->
   </footer>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Footer",
+};
 </script>
 
 <style>
